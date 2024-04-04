539ee201a86e784db64e3964162a8c71cc1f4ca4
NormProgr <norman.metzinger@gmx.de>
2024-04-04 12:38:34 +0200

    changes

---
+++ src/difference_in_difference_with_deep_learning/data_management/clean_data.py
@@ -0,0 +1,14 @@
+"""Function(s) for cleaning the data set(s)."""
+
+
+def clean_data(data):
+    """Clean the data set(s).
+
+    Args:
+        data (pd.DataFrame): The data set(s) to clean.
+
+    Returns:
+        pd.DataFrame: The cleaned data set(s).
+
+    """
+    return data
--- src/difference_in_difference_with_deep_learning/data_management/task_data_management.py
+++ src/difference_in_difference_with_deep_learning/data_management/task_data_management.py
@@ -1,6 +1,23 @@
 """Tasks for managing the data."""
 
-from pathlib import Path
-
+import pandas as pd
+import pytask
 
 from difference_in_difference_with_deep_learning.config import BLD, SRC
+from difference_in_difference_with_deep_learning.data_management import clean_data
+
+
+@pytask.mark.depends_on(
+    {
+        "script": ["clean_data.py"],
+        "data": SRC / "data" / "testdata.csv",
+    },
+)
+@pytask.mark.task
+@pytask.mark.produces(
+    BLD / "difference_in_difference_with_deep_learning" / "data" / "cleaned_data.csv",
+)
+def task_clean_data(depends_on, produces):
+    data = pd.read_csv(depends_on["data"])
+    data = clean_data(data)
+    data.to_csv(produces, index=False)
