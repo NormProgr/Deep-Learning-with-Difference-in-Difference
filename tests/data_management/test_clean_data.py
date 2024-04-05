72a54d9e6aaf5fae7cd6637229a4f841c8d08766
NormProgr <norman.metzinger@gmx.de>
2024-04-05 13:20:36 +0200

    built the basic model

--- tests/data_management/test_clean_data.py
+++ tests/data_management/test_clean_data.py
@@ -1,6 +1,7 @@
 from difference_in_difference_with_deep_learning.data_management import clean_data
 
 
-def test_clean_data():
+def test_clean_data_with_empty_data():
     data = "data"
-    assert clean_data(data) == data
+    data_info = "data_info"
+    assert clean_data(data, data_info) == "data", "data_info"
